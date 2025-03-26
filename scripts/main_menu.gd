c6c536488f794fa49a12c5e288c721e6af71b7e2
Alexander Farber <farber72@outlook.de>
2025-03-26 12:10:58 +0100

    Rotate camera pivot

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,5 +2,7 @@ extends Node3D
 
 @onready var camera_pivot: Node3D = $CameraPivot
 
+const ROTATION_SPEED = 2
+
 func _process(delta: float) -> void:
-	pass
+	camera_pivot.rotation_degrees.y += delta * ROTATION_SPEED
